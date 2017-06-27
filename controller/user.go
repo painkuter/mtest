db682a6b50b635fb8e130285eea48bad03563427
painkuter <kuter91@gmail.com>
2017-06-27 22:52:10 +0300

    render

---
+++ controller/user.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/martini-contrib/render"
+
+func PrintUser(r render.Render) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
 
 func main() {
 	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Directory: "view", // Specify what path to load the templates from.
+		//Layout:     "layout",                   // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+		Extensions: []string{".html"}, // Specify extensions to load for templates.
+		//Funcs:           []template.FuncMap{AppHelpers}, // Specify helper function maps for templates to access.
+		//Delims:          render.Delims{"{[{", "}]}"}, // Sets delimiters to the specified strings.
+		Charset:    "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
+		IndentJSON: true,    // Output human readable JSON
+		IndentXML:  true,    // Output human readable XML
+		//HTMLContentType: "application/xhtml+xml",     // Output XHTML content type instead of default "text/html"
+	}))
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
-	m.RunOnAddr(":8080")
+	m.Get("/index", func(r render.Render) {
+		r.HTML(200, "index", "jeremy")
+	})
+	m.Post("/handler", func(r render.Render) {
+		r.JSON(200, map[string]interface{}{"field": "value"})
+	})
+	m.RunOnAddr(":8088")
 }
+
+//TODO: implement auth
+//TODO: create makefile
