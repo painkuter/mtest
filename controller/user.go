b92e1d3e076e06b9f976c321f2c66294a6fd048e
painkuter <kuter91@gmail.com>
2017-06-29 07:25:07 +0300

    user class

--- controller/user.go
+++ controller/user.go
@@ -1,7 +1,55 @@
 package controller
 
-import "github.com/martini-contrib/render"
+import (
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessionauth"
+	"golang.org/x/crypto/openpgp/errors"
+)
+
+const (
+	admin = "admin"
+	pass  = "1234"
+)
+
+type UserAuth struct {
+	ID            int    `form: "-"`
+	Name          string `form: "login"`
+	PassHash      string `form: "pass"`
+	authenticated bool   `form: "-"`
+}
 
 func PrintUser(r render.Render) error {
 	return nil
+}
+
+func GenerateAnonymousUser() sessionauth.User {
+	return &UserAuth{}
+}
+
+func (u *UserAuth) Login() {
+	// Update last login time
+	// Add to logged-in user's list
+	// etc ...
+	u.authenticated = true
+}
+
+func (u *UserAuth) Logout() {
+	// Remove from logged-in user's list
+	// etc ...
+	u.authenticated = false
+}
+
+func (u *UserAuth) GetById(id interface{}) error {
+	if id != 1 {
+		return errors.ErrUnknownIssuer
+	}
+	return nil
+}
+
+func (u *UserAuth) IsAuthenticated() bool {
+	return u.authenticated
+}
+
+func (u *UserAuth) UniqueId() interface{} {
+	return u.ID
 }
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
-	"github.com/martini-contrib/binding"
 	"mtest/controller"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessionauth"
+	"github.com/martini-contrib/sessions"
 )
 
 func main() {
 	m := martini.Classic()
+	store := sessions.NewCookieStore([]byte("secret123"))
+	store.Options(sessions.Options{
+		MaxAge: 0,
+	})
+	m.Use(sessions.Sessions("my_session", store))
+	m.Use(sessionauth.SessionUser(controller.GenerateAnonymousUser))
+	sessionauth.RedirectUrl = "/new-login"
+	sessionauth.RedirectParam = "new-next"
 	m.Use(render.Renderer(render.Options{
 		Directory: "view", // Specify what path to load the templates from.
 		//Layout:     "layout",                   // Specify a layout template. Layouts can call {{ yield }} to render the current template.
@@ -30,8 +41,10 @@ func main() {
 	//	r.JSON(200, map[string]interface{}{"field": "value"})
 	//})
 	m.Post("/handler", binding.Bind(controller.PostRequest{}), controller.Handler)
+	m.Post("/auth", binding.Bind(controller.UserAuth{}))
 	m.RunOnAddr(":8088")
 }
 
 //TODO: implement auth
+//TODO: implement sessions and sessionauth
 //TODO: create makefile
